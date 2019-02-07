7a4637e04d2f7eeaba29610a008437f980ccfae5
Corentin49 <44872130+Corentin49@users.noreply.github.com>
2019-02-07 14:12:22 +0100

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,11 +1,5 @@
-package determinant;
 
-import java.util.Scanner;
 
-public class Main {
-
-	public static void main(String[] args) {
-		// TAILLE
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Veuillez saisir le n :");
 		int nb = sc.nextInt();
@@ -16,6 +10,7 @@ public class Main {
 		int ligne;
 		int colonne;
 
+		// Cr�ation de la matrice
 		for (int l = 0; l < nb; l++) {
 			for (int c = 0; c < nb; c++) {
 				ligne = l + 1;
@@ -27,11 +22,12 @@ public class Main {
 			}
 		}
 
-		System.out.println("1 - Je choisie sur quel ligne/colonne calculer \n2 - Automatique");
+		System.out.println(
+				"1 - Je choisie sur quel ligne/colonne calculer \n2 - Automatique\n3 - Automatique Avec insertion zero\n4 - Calcule déterminant exposant m\n5 - Inverse\n6 - Cramer");
 		int choixCalcul = sc.nextInt();
 
-		while (choixCalcul != 1 && choixCalcul != 2) {
-			System.out.println("Erreur Choisir entre 1 et 2");
+		while (choixCalcul != 1 && choixCalcul != 2 && choixCalcul != 3 && choixCalcul != 4 && choixCalcul != 5 && choixCalcul!= 6) {
+			System.out.println("Erreur Choisir entre 1 et 2 et 3 et 4 et 5 et 6");
 			choixCalcul = sc.nextInt();
 		}
 
@@ -46,28 +42,47 @@ public class Main {
 			}
 
 			if (choixCOuL == 2) {
-
 				System.out.println("Choisir le numero de la ligne");
 				int choixL = sc.nextInt() - 1;
-
 				while (choixL < 1 && choixL > matrice.length) {
 					System.out.println("Erreur choisir un nombre entre 1 et " + matrice.length);
 					choixL = sc.nextInt();
 				}
+				System.out.println("Le d�terminant est : " + calculerDeterminantParLigne(choixL, matrice));
 			} else if (choixCOuL == 1) {
 				System.out.println("Choisir le numero de la colonne");
 				int choixC = sc.nextInt() - 1;
-
 				while (choixC < 1 && choixC > matrice.length) {
 					System.out.println("Erreur choisir un nombre entre 1 et " + matrice.length);
 					choixC = sc.nextInt();
 				}
+				System.out.println("Le d�terminant est : " + calculerDeterminantParLigne(choixC, matrice));
 			}
 
-		} else if (choixCalcul == 2) {
+		} else if (choixCalcul == 2) { // Automatique
+			System.out.println("Le d�terminant est : " + calculDeterminantAutomatique(matrice));
+		} else if (choixCalcul == 3) {
+			System.out.println("Le d�terminant est : " + calculDeterminantAvecInsertionZero(matrice));
+		} else if (choixCalcul == 4) {
 
+			System.out.println("Choisir la puissance");
+			int choixM = sc.nextInt();
+			while (choixM < 1) {
+				System.out.println("Erreur choisir un nombre supp�rieur ou �gale a 1");
+				choixM = sc.nextInt();
+			}
+			calculerDeterminantExposantN(choixM, matrice);
+		} else if (choixCalcul == 5) {
+			calculerMatriceInverse(matrice);
+		} else if (choixCalcul == 6){
+			float b[] = new float[nb];
+			for (int i = 0; i < b.length; i++) {
+				System.out.println("Choisir la valeur numéro "+ (i+1) +" de B");
+				float choixB = sc.nextFloat();
+				b[i] = choixB;
+			}
+			// appelle fonction Cramer
 		}
+		// float[][] matrice = { { 2, 2, -4 }, { 2, 1, -3 }, { 1, 0, 0} };
 
-	}
-
-}
+	
